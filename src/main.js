f008f944530daab7b40e5692d251bf426d9ca965
hanshirong <1418264042@qq.com>
2019-07-07 22:14:50 +0800

    QAQ

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './icons'
 import Api from './api/index';
 import store from './store';
+import Blob from './excel/Blob.js'
+import Export2Excel from './excel/Export2Excel.js'
+
 Vue.use(ElementUI,{size:'small',zIndex:3000});
 
 Vue.config.productionTip = false
