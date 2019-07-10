cdc42b9a1c9ff53bfb08454db9c4bbef33b53b23
hanshirong <1418264042@qq.com>
2019-07-10 20:30:37 +0800

    okk

--- src/components/storageLocation.vue
+++ src/components/storageLocation.vue
@@ -1,20 +1,14 @@
 <template>
-    <div class="app-container">
-      
-       <h2>库位管理</h2>
-        
-        <el-table
-            :data="storageLocationData"
-            @expand-change="handleUpdate"
-             style="width: 100%; "
-            
-            >
-            <!--展开-->
-                <el-table-column  type="expand"  >
-                <template slot-scope="props">
-                    
+  <div class="app-container">
+    <h2>库位管理</h2>
+
+    <el-table :data="storageLocationData" @expand-change="handleUpdate" style="width: 100%; " :row-class-name="tableRowClassName">
+      <!--展开-->
+      <el-table-column type="expand">
+        <template slot-scope="row">
+          
                     <el-table
-                        :data="propsData"
+                        :data="propsData[row.$index]"
                         
                         border
                         style="width: 100%">
@@ -41,23 +35,14 @@
                             
                             </el-table-column>
                             
-                    </el-table>
-                
-                </template>
-                </el-table-column>
-            <!--库位号托数表格-->
+          </el-table>
+        </template>
+      </el-table-column>
+      <!--库位号托数表格-->
 
-                <el-table-column
-                    prop="name"
-                    label="库位号"
-                    width="180" >
-                </el-table-column>
-                <el-table-column
-                    prop="trayNum"
-                    label="托数">
-                
-                </el-table-column>
-            <!--添加删除
+      <el-table-column prop="name" label="库位号" width="180"></el-table-column>
+      <el-table-column prop="trayNum" label="托数"></el-table-column>
+      <!--添加删除
                 <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
                     <template slot-scope="{row}">
                     <el-button type="primary" size="mini">
@@ -68,127 +53,113 @@
                     </el-button>
                     </template>
                     
-                </el-table-column>-->
-        </el-table>
-       
-    </div>
+      </el-table-column>-->
+    </el-table>
+  </div>
 </template>
 <script>
 import axios from "axios";
 
 export default {
-    data(){
-        return{
-           
-            i:0,
-            storageNum:'',
-            propsData:[{
-                Num:'',
-                serial:'',
-                name:'',
-                count:'',
-                entryAt:'',
-                mobileTicket:'',
-                direction:'',
-                
-            },
-          ],
-            storageLocationData: [{
-                name:'',
-                trayNum:'',  
-            }]
-        }
-    },
-    
-    mounted:function(){
-      let that = this;
-      
-      axios({
-        method: "GET",
-        url: "api/api/v1/location",
-        headers:{
-            authorization: "Bearer "+ that.$store.state.token
-        }
-     })
-      .then(function(response) {
-       
-        that.storageLocationData=response.data;//库位号以及托数
-         
+  data() {
+    return {
+      i: 0,
+      storageNum: "",
+      propsData: [[{
+          Num: "",
+          serial: "",
+          name: "",
+          count: "",
+          entryAt: "",
+          mobileTicket: "",
+          direction: ""
+        },]
         
+      ],
+      storageLocationData: [
+        {
+          name: "",
+          trayNum: ""
+        }
+      ]
+    };
+  },
+
+  mounted: function() {
+    let that = this;
+
+    axios({
+      method: "GET",
+      url: "api/api/v1/location",
+      headers: {
+        authorization: "Bearer " + that.$store.state.token
+      }
+    })
+      .then(function(response) {
+        that.storageLocationData = response.data; //库位号以及托数
       })
       .catch(function(error) {
         console.log(error);
-         if(error =="Error: Request failed with status code 401")
-           that.$router.push({ path: "/" });
+        if (error == "Error: Request failed with status code 401")
+          that.$router.push({ path: "/" });
       });
-},
-    methods:{
-        handleChange(val) {
-        console.log(val);
-      },
-        handleFilter() {
-        console.log('filter');
-      },
-      handleCreate() {
-       console.log('create');
+  },
+  methods: {
+    handleChange(val) {
+      console.log(val);
     },
-    handleUpdate(row){
-         let that = this;
-         console.log(row.trayNum);
-        for(var i=0;i<row.trayNum;i=i+1){
-            that.$set(that.propsData,i,{ Num:"托"+i,});
-        }
-        
+    handleFilter() {
+      console.log("filter");
+    },
+    handleCreate() {
+      console.log("create");
+    },
+    handleUpdate(row) {
+      let that = this;
+      var index=row.index;
       
-       
-       
-            
-        
-            
-        
-        /*
-         axios({
-            method: "GET",
-            url: "api/api/v1/location/"+that.storageLocationData[row].id,
-            headers:{
-                authorization: "Bearer "+ that.$store.state.token
-            },
-            
-         })
-      .then(function(response) {
-        console.log(response);
-        if(response.data.trays !=null){
-            that.propsData=response.data.trays.items;
-        }
-            
-       
-      })
-      .catch(function(error) {
-        //console.log(error.message);
-        alert(error);
-        if(error==="Error: Request failed with status code 401")
-           that.$router.push({ path: "/" });
-        
+      that.$set(that.propsData,index,[{Num:"托index"}]);
+      
+     
 
-      });*/
-    }
-    }
-}
+      
+        for(var i=1;i<=row.trayNum;i=i+1){
+            that.$set(that.propsData[index],i-1,{ Num:"托"+i,});
+        }
+         console.log(that.propsData[index]);
+        /*
+      axios({
+        method: "GET",
+        url: "api/api/v1/location/" + row.id,
+        headers: {
+          authorization: "Bearer " + that.$store.state.token
+        }
+      })
+        .then(function(response) {
+          console.log(response);
+
+          that.propsData = response.data.packs;
+        })
+        .catch(function(error) {
+          alert(error);
+          if (error == "Error: Request failed with status code 401")
+            that.$router.push({ path: "/" });
+        });*/
+    },
+    tableRowClassName ({row, rowIndex}) {
+       //获取每行索引
+        row.index = rowIndex;
+    },
+  }
+};
 </script>
 <style>
-.app-container{
-    display: flex;
-    margin-top:30px;
-    margin-left:80px;
-    margin-right: 80px;
-    flex-direction: column;
-
-}
-.el-table{
-    
-}
-.el-table-column{
-   
+.app-container {
+  display: flex;
+  margin-top: 30px;
+  margin-left: 80px;
+  margin-right: 80px;
+  flex-direction: column;
 }
 </style>
 
